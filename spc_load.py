5bf9b8cf70f3aacb8f1bdf298cb2b21b032f904f
bridgebook112 <bridgebook1999@gmail.com>
2022-03-19 12:31:45 +0900

    test change

---
+++ spc_load.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 18:12:40 2022
+
+@author: Hashimoto
+"""
+
+from pyspectra.readers.read_spc import read_spc
+import spc
+#spc=read_spc("F:/Research/BIO/OpticalEmissionSpectroscopy/plasajet/USB/jet1.0cm_shift.spc")
+s=spc.File("E:/7-9/control1.spc")
